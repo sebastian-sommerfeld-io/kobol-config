fb47d991fb4e5fb666070dc0d849e63d771e6f32
sebastian <sommerfeld.sebastian@gmail.com>
2022-03-05 00:30:52 +0100

    feat: ssh-connect to host https://sommerfeld-io.atlassian.net/browse/SIO-129

--- src/main/vagrantboxes/pegasus/provision/ssh-keys.sh
+++ src/main/vagrantboxes/pegasus/provision/ssh-keys.sh
@@ -2,8 +2,7 @@
 # @file ssh-keys.sh
 # @brief Provisioning script for Vagrantbox pegasus.
 #
-# @description The scripts create some ssh-keys to use with Vagrantbox ``pegasus`` and adds the key to the ssh agent.
-# The other existing private keys are added as well (keys are copied into the Vagrantbox during provisioning).
+# @description The scripts create some ssh-keys to use with Vagrantbox ``pegasus`` for user ``vagrant``
 #
 # IMPORTANT: DON'T RUN THIS SCRIPT DIRECTLY - Script is invoked by Vagrant during link:https://www.vagrantup.com/docs/provisioning[provisioning].
 #
@@ -14,22 +13,6 @@
 DIR="/home/vagrant/.ssh"
 KEY="id_rsa"
 
-echo -e "$LOG_INFO Start the ssh-agent in background"
-eval "$(ssh-agent -s)"
-
 echo "[INFO] Create $DIR/$KEY"
-ssh-keygen -t rsa -N "" -C "sebastian@sommerfeld.io" -f "$DIR/$KEY"
-
-#chown vagrant:vagrant "$DIR/$KEY"
-#chown vagrant:vagrant "$DIR/$KEY.pub"
-
-echo -e "$LOG_INFO Add keys to the ssh-agent"
-ssh-add "$DIR/$KEY"
-
-for k in /home/vagrant/.ssh/*.key
-do
-  echo "[INFO] Add key: $k"
-  ssh-add "$k"
-done
-
+ssh-keygen -m PEM -t rsa -b 4096 -N "" -C "sebastian@sommerfeld.io" -f "$DIR/$KEY"
 echo "[DONE] Finished ssh key setup"
